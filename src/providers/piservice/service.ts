247ed38467ad688e029d06d0ec3e61519b1f78b3
eatmyball <denghliu@126.com>
2019-07-03 16:30:28 +0800

    更新地址到生产机

--- src/providers/piservice/service.ts
+++ src/providers/piservice/service.ts
@@ -23,13 +23,13 @@ import { LoadingController, AlertController } from 'ionic-angular';
 export class Service {
 
     //config*********************************************************
-    version = "1.0.4(20190626001)";
+    version = "1.0.5(20190703002)";
     mode = "test";//dev, test, prod
     dev = "dev";
     test = "test";
     prod = "prod";
     devPrefix = "/apid";
-    testPrefix = "http://happyship.wisebox.com.cn:8080/happyship";
+    testPrefix = "http://47.96.150.71/mw";
     // testPrefix = "http://172.28.112.208:8080/yozan"
     // testPrefix = "http://transport.shyozan.com/yozantest";
     // prodPrefix = "http://hrwechat-qas.svw.cn";
